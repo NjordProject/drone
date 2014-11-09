b3d64a5b4beb7225987e69e92926bc3726ede001
Henriojord <henriojord@eisti.eu>
2014-11-09 21:11:51 +0100

    fixing comments

--- Motor/bccm01b.cpp
+++ Motor/bccm01b.cpp
@@ -5,18 +5,18 @@ Created by Njord team, November 3, 2014.
 
 #include "bccm01b.h"
 
-Bccm01b::Bccm01b (int pin) {
+Bccm01b::Bccm01b ( int pin ) {
 	_pin = pin;
 	_value = 0;
-	pinMode (_pin, OUTPUT);
+	pinMode ( _pin, OUTPUT );
 }
 
-int Bccm01b::controlSpeed (int value) {
-	if (value < 0 || value > 255) {
+int Bccm01b::controlSpeed ( int value ) {
+	if ( value < 0 || value > 255 ) {
 		return 1; //Report a value error
 	} else {
 		_value = value;
-		analogWrite (_pin, _value);
+		analogWrite ( _pin, _value );
 		return 0;
 	}
 }
